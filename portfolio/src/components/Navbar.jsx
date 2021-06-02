4821ef15fbd0d014f26eed9ea319000c3540ccf4
franciscotov <frank.ronaldo_17@hotmail.com>
2021-06-02 00:54:35 -0300

    ultimos cambios

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import styles from './Navbar.module.scss'
+import styles from './Navbar.module.scss';
+import { GrLinkedinOption } from 'react-icons/gr';
+import {FaGithub} from 'react-icons/fa';
+import {FaFreeCodeCamp} from 'react-icons/fa'
 
+const icon = (ic) => {
+    return () => ic;
+}
 
 const Navbar = () => {
     let sections = ['Home', 'About', 'Skills', 'Experience', 'Contact', 'Portfolio']
     , redes = [
-        {name: 'Li.', link: 'https://www.linkedin.com/in/franciscotov/'}, 
-        {name: 'Git.', link: 'https://github.com/franciscotov'}
-    ];
+        {name: icon(<GrLinkedinOption></GrLinkedinOption>), link: 'https://www.linkedin.com/in/franciscotov/'}, 
+        {name: icon(<FaGithub></FaGithub>), link: 'https://github.com/franciscotov'},
+        {name: icon(<FaFreeCodeCamp></FaFreeCodeCamp>), link: 'https://www.freecodecamp.org/franciscotov'},
+        // {name: 'Free', link: 'https://www.freecodecamp.org/franciscotov'},
+        // {name: 'Free', link: 'https://www.freecodecamp.org/franciscotov'}
+    ],
+    name = 'Francisco Tovar';
 
     const [active, setActive] = useState(false);
 
@@ -18,27 +28,30 @@ const Navbar = () => {
     }
     return (
         <div className={styles.nav}>
-            <div className={styles.containerName}><p>Francisco Tovar</p></div>
-            <div className={styles.containerAcor}>
-                <button
-                    onClick={(e) => eventHandler(e)}
-                    className={active ? 'active' : 'inactive'}
-                    aria-expanded={active ? 'true' : 'false'}
-                    // aria-controls={'sect-' + indexCount(index)}
-                    aria-disabled={active? 'true' : 'false'}
-                    // tabIndex={indexCount(index)}
-                >
-                    <span className={styles.titlewrapper}>
-                        <span className={active ? styles.plus : styles.minus}></span>
-                    </span>
-                </button>
+            <div className={styles.containerName}><p>{name}</p></div>
+            <div className={styles.containerMenu}>
+                <div className={styles.containerButton}>
+                    <button
+                        onClick={(e) => eventHandler(e)}
+                        className={active ? `${styles.active}` : `${styles.inactive}`}
+                        aria-expanded={active ? 'true' : 'false'}
+                        // aria-controls={'sect-' + indexCount(index)}
+                        aria-disabled={active? 'true' : 'false'}
+                        // tabIndex={indexCount(index)}
+                    >
+                        <span className={styles.titlewrapper}>
+                            <span className={active ? styles.plus : styles.minus}></span>
+                        </span>
+                    </button>
+                </div>
                 {/* <div className={active ? styles.open: styles.close} className={styles.socialMenu}> */}
-                <div  className={active ? styles.open: styles.close}>
-                    {redes.map((ele,i) =>{
-                        return <div className={styles.red}><a href={ele.link} target='_blank' rel="noreferrer">{ele.name}</a></div>
+                {/* <div  className={styles.containerAcor}> */}
+                <div className={active ? `${styles.containerAcor} ${styles.open}` : styles.containerAcor}>
+                    {redes.map((ele, i) => {
+                        return <div className={styles.red}><a className={styles.icon} href={ele.link} target='_blank' rel="noreferrer">{ele.name()}</a></div>
                     })}
                 </div>
-                <div className={styles.menuHolder}>
+                <div className={active ? `${styles.menuHolderActive} ${styles.menuHolder}` : `${styles.menuHolder}`}>
                     <nav className={styles.headerMenu}>
                         <ul className={styles.menu1}>
                             {sections.map((ele,i) =>{
