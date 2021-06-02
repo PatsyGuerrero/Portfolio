688f47867d43cb220105fbe0050b5ecdb39f0f7c
patsyguerrero <patsy.guerrero95@gmail.com>
2021-06-01 22:58:32 -0500

    footer css

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import styles from './Footer.module.scss'
+import styles from './Footer.module.scss';
+import {GrLinkedinOption} from 'react-icons/gr';
+import {FaGithub, FaFreeCodeCamp} from 'react-icons/fa'
+
 const Footer = () => {
     
         return (
             <section className={styles.container}>
-                <div>
-                    
+                <div className={styles.elements}>
+                    <div className={styles.icon}>
+                        <GrLinkedinOption className={styles.size}/>
+                    </div>
+                    <div className={styles.icon}>
+                        <FaGithub className={styles.size}/>
+                    </div>
+                    <div className={styles.icon}>
+                        <FaFreeCodeCamp className={styles.size}/>
+                    </div>
                 </div>
             </section>
         );
