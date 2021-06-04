6aeaa43351ac6b3f6cd00208561ed2f5d7be92c4
franciscotov <frank.ronaldo_17@hotmail.com>
2021-06-04 18:24:34 -0300

    ultimos cambios en componente photo, responsive completo

--- portfolio/src/components/Photo.jsx
+++ portfolio/src/components/Photo.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Download from './Download';
 import styles from './Photo.module.scss';
-
+import img from "../assets/img/photo.JPG";
 
 const Photo = () => {
     return (
         <>
         <div className={styles.container}>
             <div className={styles.photoContainer}>
-                <div className={styles.photo}>hola</div>
+                {/* <div className={styles.photo}>hola</div> */}
+                <img src={img} alt="" className={styles.photo}  width="25vw" height="40vw"/>
+                <Download />
             </div>
-            <Download />
         </div>
         </>
     );
